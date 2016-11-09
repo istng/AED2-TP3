35ea13b833a93f2c66155b6dafeb1bfd67151a16
Pato <pato.sabogal@hotmail.com>
2016-11-09 18:52:17 -0300

    terminado Juego.h, enrealidad ni compila, pero falta DiccString y HeapModificable

--- Base/Juego.h
+++ Base/Juego.h
@@ -3,11 +3,22 @@
 
 #include "TiposJuego.h"
 #include "mapa.hpp"
+#include "HeapModificable.h"
+struct pokes{
+
+    Pokemon poke;
+    Nat cant;
+
+}
+
+template<class T> 
+typedef Vector<Vector<T> > Matriz; 
+
 class Juego{
 
     public:
         
-        Juego(Mapa m);
+        Juego(Mapa& m);
 
         ~Juego();
 
@@ -21,33 +32,107 @@ class Juego{
         
         void Moverse(Jugador j, Coordenada c);
 
-        Mapa Mapa();
+        Mapa Mapa() const;
 
-        //iTConj::Conj<Jugadores> Jugadores();
+        const_Iterador::Conj<Jugadores> Jugadores() const;
 
-        bool EstaConectado(Jugador j);
+        bool EstaConectado(Jugador j) const;
 
-        Nat Sanciones(Jugador j);
+        Nat Sanciones(Jugador j) const;
 
-        Coordenada Posicion(Jugador j);
+        Coordenada Posicion(Jugador j) const;
 
-        //itConj::Conj<Pokemon,Nat> Pokemons(Jugador j);
+        const_Iterador::Conj<tupla> Pokemons(Jugador j) const;
 
-        Conj<Jugadores>::Expulsados();
+        Conj<Jugadores>::Expulsados() const;
 
-        Conj<Pokemon> PosConPokemon();
+        Conj<Coordenada> PosConPokemon() const;
 
-        Pokemon PokemonEnPos(Coordenada c);
+        Pokemon PokemonEnPos(Coordenada c) const;
 
-        Nat CantMovimientosParaCaptura(Coordenada c);
+        Nat CantMovimientosParaCaptura(Coordenada c) const;
         
-        bool PuedoAgregarPokemon(Coordenada c);
+        bool PuedoAgregarPokemon(Coordenada c) const;
         
-        bool HayPokemonCercano(Coordenada c);
+        bool HayPokemonCercano(Coordenada c) const;
 
-        Coordenada PosPokemonCercano(Coordenada c);
+        Coordenada PosPokemonCercano(Coordenada c) const;
 
-        Conj<Jugadores> EntrenadoresPosibles(Coordenada c, Conjunto<Jugadores> js);
+        Conj<Jugadores> EntrenadoresPosibles(Coordenada c, Conjunto<Jugadores> js) const;
 
-        Nat IndiceDeRareza(Pokemon p);
+        Nat IndiceDeRareza(Pokemon p) const;
+
+        friend ostream& operator<<(ostream& os, const Juego& j){
+            j.mostrar(os);
+            return os;
+        }
+
+    private:
+        
+        struct infoJug{
+
+            bool conectado;
+
+            Nat sanciones;
+
+            Coordenada posicion;
+
+            Conj<pokes> pokemons;
+
+            DiccString<Iterador::Conj<pokes> > pokesRapido;
+
+            //Iterador::HeapModificable<Nat, Jugador> prioridad;
+            
+            Iterador::Conj<Jugador> posMatriz;
+          
+            Iterador::Conj<Jugador> lugarNoExpulsado;
+
+        }
+
+        struct infoMatrizPoke{
+            
+            bool hayPoke;
+
+            Iterador::Dicc<Coordenada, infoCoord> iterador;
+
+        }
+
+        struct infoCoord{
+            
+            Pokemon tipo;
+
+            Nat cantMovCap;
+
+            //HeapModificable<Nat, Jugador> colaDePrioridad;
+
+        }
+
+        struct infoPoke{
+        
+            Nat cant;
+
+            Conj<Iterrador::Dicc<Coordenada, infoCoor> >
+
+        }
+
+        Mapa mapa;
+        
+        Vector<infoJug> jugadores;
+ 
+        Conj<Jugadores> jugadoresNoExpulsados;
+
+        Conj<Jugadores> expulsados;
+
+        Matriz<Conj<Jugadores> > matrizJugadores;        
+
+        Matriz<infoMatrizPoke> matrizPokemons;
+
+        Dicc<Coordenada, infoCoordenada> posPokemons;
+
+        DiccString<infoPoke> pokemonesTotales;
+
+        Nat cantidadPokeTotal;
+
 }
+
+#endif // JUEGO_H_
