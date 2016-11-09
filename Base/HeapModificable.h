df6a51bd415625b791ef1d24bffd85370342a4e0
lucascarbonelli <luc92ac@hotmail.com>
2016-11-09 20:17:27 -0300

    heap modificable punto hache

---
+++ Base/HeapModificable.h
@@ -0,0 +1,72 @@
+#ifndef TP3_HEAPMODIFICABLE_H
+#define TP3_HEAPMODIFICABLE_H
+
+#include "aed2/TiposBasicos.h"
+
+class HeapModificable
+{
+	public:
+	
+	    class Iterador;
+		class const_Iterador;
+	
+		struct JugadorHeap {
+			Nat cantPokes;
+			Nat id;
+		}
+		
+		bool operator<(const JugadorHeap& a, const JugadorHeap& b); // reemplaza función EsMayor?
+	
+	// Observadores
+		bool esVacia(); // Vacia?()
+		const JugadorHeap& proximo() const;
+		
+	// Generadores
+		HeapModificable(); // Generador Vacia()
+		HeapModificable::Iterador Encolar(const JugadorHeap& a);
+		
+	// Otras operaciones
+		void desencolar();
+	
+	// Iterador
+		class Iterador
+		{
+			public:
+				Iterador();	// CrearIt()
+				bool haySiguiente() const;
+				JugadorHeap& siguiente() const;
+				void eliminarSiguiente();
+				void agregarComoSiguiente(const JugadorHeap& a);
+		}
+		
+		class const_Iterador
+		{
+			public:
+				Iterador();	// CrearIt()
+				bool haySiguiente() const;
+				const JugadorHeap& siguiente() const;
+		}
+		
+	private:
+			
+		struct Nodo
+		{
+			JugadorHeap elemento;
+			Nat ramaMasCorta;
+			Nat ramaMasLarga;
+			Nodo* hijoIzq;
+			Nodo* hijoDer;
+			Nodo* padre;
+		}
+		
+		Nodo* tope;
+		
+		Nodo* ultimoNodo() const;
+		Nodo* futuroPadre() const;
+		void corregirProfundidad(Nodo* p);
+		void siftDown(Nodo* p);
+		void siftUp(Nodo* p);
+		void intercambio(Nodo* padre, Nodo* hijo);
+}
+
+#endif
