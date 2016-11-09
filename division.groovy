c441c6fefe08f714b4f13eccbdbd3b96f6a2e9b9
istng <ivanstng@mail.com>
2016-11-09 15:31:43 -0300

    agrego la division groovy

---
+++ division.groovy
@@ -0,0 +1,31 @@
+
+
+coordenada trivial -> Juan
+
+mapa -> probablemente Juan
+
+heap -> Iván y Lucas
+
+diccString -> Iván
+
+redactar la clases -> Lucas
+
+observadores trivial -> Lucas
+
+moverse -> Juan (no necesariamente vos solo)
+
+crearJuego trivial -> Iván
+
+crearMatrizJug -> Lucas
+
+crearMatrizPokes -> Lucas
+
+agregarPokemon -> Iván
+
+agregarJugador -> Pato
+
+conectarse -> Pato
+
+desconectarse -> Pato
+
+otras operaciones -> Pato
