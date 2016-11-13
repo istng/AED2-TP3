066df1c5f288f6f9eea6332868da6be4c4c0cda6
juan <juanmanuelbaldonado@gmail.com>
2016-11-13 11:54:05 -0300

    coordenada y mapa

--- Base/TiposJuego.h
+++ Base/TiposJuego.h
@@ -3,9 +3,11 @@
 
 #include "aed2/TiposBasicos.h"
 
+using namespace aed2;
+
 typedef unsigned int Jugador;
 typedef std::string Pokemon;
- 
+
 struct Coordenada
 {
 	Nat latitud;
@@ -15,12 +17,71 @@ struct Coordenada
 	}
 };
 
+Nat latitud(const Coordenada& c){
+	return c.latitud;
+}
+
+Nat longitud(const Coordenada& c){
+	return c.longitud;
+}
+
+
+Nat DistEuclidea(const Coordenada& c1,const Coordenada& c2){
+	Nat lon = 0;
+	Nat lat = 0;
+	if (c1.longitud > c2.longitud)
+	{
+		lon = (c1.longitud-c2.longitud)*(c1.longitud-c2.longitud);
+	}
+	else
+	{
+		lon = (c2.longitud-c1.longitud)*(c2.longitud-c1.longitud);
+	}
+
+	if (c1.latitud > c2.latitud)
+	{
+		lat = (c1.latitud-c2.latitud)*(c1.latitud-c2.latitud);
+	}
+
+	else
+	{
+		lat = (c2.latitud-c1.latitud)*(c2.latitud-c1.latitud);
+	}
+
+	return (lat + lon);
+
+
+}
+
+
+Coordenada CoordenadaArriba(const Coordenada& c){
+	return Coordenada(latitud(c)+1,longitud(c));
+}
+
+Coordenada CoordenadaAbajo(const Coordenada& c){
+	return Coordenada(latitud(c)-1,longitud(c));
+} 
+
+Coordenada CoordenadaALaDerecha(const Coordenada& c){
+	return Coordenada(latitud(c),longitud(c)+1);
+}
+Coordenada CoordenadaALaIzquierda(const Coordenada& c){
+	return Coordenada(latitud(c),longitud(c)-1);
+}
+
+      
+
 bool operator == (const Coordenada& c1, const Coordenada& c2){
  return c1.latitud == c2.latitud and c1.longitud == c2.longitud;
 }
 
+
 bool operator != (const Coordenada& c1, const Coordenada& c2){
  return not (c1 == c2);
+}
+
+std::ostream& operator<<(std::ostream& os, const Coordenada& c){
+	os << '(' << c.latitud << ',' << c.longitud << ')';
 }
 
 #endif
---
+++ Base/mapa.hpp
@@ -0,0 +1,299 @@
+#ifndef MAPA_H_
+#define MAPA_H_
+
+#include "TiposJuego.h"
+#include "aed2/TiposBasicos.h"
+#include "aed2/Vector.h"
+#include "aed2/Conj.h"
+
+using namespace aed2;
+
+class Mapa
+{
+	public:
+		//Generadores
+	    Mapa();
+		void agregarCoor(const Coordenada& c);
+		//Destructor
+		//void ~Mapa();
+
+		//Observadores
+
+		Conj<Coordenada>::const_Iterador coordenadas() const ;
+		bool posExistente (const Coordenada& c) const ;
+		bool hayCamino (const Coordenada& c , const Coordenada& cprima)const;
+		bool hayCamino2 (const Coordenada& c , const Coordenada& cprima)const;
+		bool ExisteCamino(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const;
+		bool ExisteCaminoPorArriba(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const ;
+		bool ExisteCaminoPorAbajo(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const;
+		bool ExisteCaminoPorIzquierda(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const;
+		bool ExisteCaminoPorDerecha(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs )const ;
+
+
+
+
+	private:
+
+		//Vector<Vector<Info>> columnas;
+		//Conj<Coordenada> coords;
+
+		/*struct Fila
+		{
+			vector<Info> fila;	
+		};*/
+
+		struct Info
+		{
+			bool _esta;
+			Conj<Coordenada> _adyacentes;
+			Info(): _esta(false), _adyacentes(){}				
+			
+
+		};
+
+
+
+		Vector<Vector<Info> > columnas;
+		Conj<Coordenada> coords;
+
+	
+};
+
+
+Mapa::Mapa(): columnas(),coords(){
+}
+
+Conj<Coordenada>::const_Iterador Mapa::coordenadas() const{
+
+	return (this->coords).CrearIt();
+
+}
+
+bool Mapa::posExistente(const Coordenada& c) const{
+
+	if (c.longitud <= this->columnas.Longitud() + 1 )
+	{
+		Vector<Info> fila = this->columnas[c.longitud];
+		if (c.latitud < fila.Longitud())
+		{
+			return fila[c.latitud]._esta;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return false;
+}
+
+void Mapa::agregarCoor(const Coordenada& c){
+
+	this->coords.Agregar(c);
+
+	if (this->columnas.Longitud() < longitud(c) + 1) 
+	{
+		int i = this->columnas.Longitud();
+		while (i <= longitud(c) + 1)
+		{
+			Vector<Info> v;
+			this->columnas.AgregarAtras(v);
+			i++;
+		}
+	}
+
+	Vector<Info>* fila = &(this->columnas[longitud(c)]);
+	if (fila->Longitud() < latitud(c) + 1)
+	{
+		int j = fila->Longitud();
+		while(j <= latitud(c) + 1)
+		{
+			fila->AgregarAtras(Info());
+			j++;
+		}
+	}
+
+	Info* Info = & (this->columnas[longitud(c)][latitud(c)]);
+	Info->_esta = true;
+
+	Conj<Coordenada>::const_Iterador it = this->coordenadas();
+
+	while (it.HaySiguiente())
+	{
+		Coordenada cprima = it.Siguiente();
+		bool hayCamino = this->hayCamino2(c,cprima);
+		bool enRango = DistEuclidea(c,it.Siguiente()) < 100;
+
+		if ( hayCamino && enRango && c != cprima )
+		{
+			this->columnas[longitud(cprima)][latitud(cprima)]._adyacentes.AgregarRapido(c);
+			Info->_adyacentes.AgregarRapido(cprima);
+		}
+
+		it.Avanzar();
+	}
+
+	std::cout << std::endl << this->coords << std:: endl;
+
+}
+
+bool Mapa::hayCamino (const Coordenada& c , const Coordenada& cprima)const {
+
+	if (DistEuclidea(c,cprima))
+	{
+		return (this->columnas[longitud(c)][latitud(c)]._adyacentes).Pertenece(cprima);
+	}
+	else
+	{
+		return this->hayCamino2(c,cprima);
+	}
+
+}
+
+bool Mapa::hayCamino2 (const Coordenada& c , const Coordenada& cprima) const{
+
+	Conj<Coordenada> coords = this->coords;
+	return this->ExisteCamino(c,cprima,coords);
+
+}
+
+bool Mapa::ExisteCamino(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const{
+
+	if (c == cprima)
+	{return true;}
+	else
+	{
+		if (cs.EsVacio())
+		{return false;}
+		else
+		{
+			bool arriva = this->ExisteCaminoPorArriba(c,cprima,cs);
+			bool abajo = this->ExisteCaminoPorAbajo(c,cprima,cs);
+			bool izquierda = this->ExisteCaminoPorIzquierda(c,cprima,cs);
+			bool derecha = this->ExisteCaminoPorDerecha(c,cprima,cs);
+
+			return (arriva or abajo or izquierda or derecha);
+		}
+	}
+}
+
+
+bool Mapa::ExisteCaminoPorArriba(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const {
+
+	
+	
+		if (cs.Pertenece(CoordenadaArriba(c)))
+		{
+			cs.Eliminar(CoordenadaArriba(c));
+			return this->ExisteCamino(CoordenadaArriba(c),cprima,cs);
+		}
+		else
+		{
+		return false;
+		}
+	
+	
+
+}
+
+
+bool Mapa::ExisteCaminoPorAbajo(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const {
+
+	if (latitud(c)>0)
+	{
+		if (cs.Pertenece(CoordenadaAbajo(c)))
+		{
+			cs.Eliminar(CoordenadaAbajo(c));
+			return this->ExisteCamino(CoordenadaAbajo(c),cprima,cs);
+
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+
+bool Mapa::ExisteCaminoPorIzquierda(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const {
+
+	if (longitud(c)>0)
+	{
+		if (cs.Pertenece(CoordenadaALaIzquierda(c)))
+		{
+			cs.Eliminar(CoordenadaALaIzquierda(c));
+			return this->ExisteCamino(CoordenadaALaIzquierda(c),cprima,cs);
+
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
+
+
+}
+
+bool Mapa::ExisteCaminoPorDerecha(const Coordenada& c, const Coordenada& cprima, Conj<Coordenada>& cs ) const {
+
+	if (cs.Pertenece(CoordenadaALaDerecha(c)))
+	{
+		cs.Eliminar(CoordenadaALaDerecha(c));
+		return this->ExisteCamino(CoordenadaALaDerecha(c),cprima,cs);
+
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+
+
+std::ostream& operator<<(std::ostream& os, const Mapa& m){
+	os <<"Coordenadas:" ;
+	
+	/*	
+	for (int i = 0; i < this->columnas.Longitud; ++i)
+	{
+		if (this->columnas[i].Longitud() > max_Y )
+		{
+			max_Y = this->columnas[i].Longitud();
+		}
+	}
+
+	for (int i = 0; i < max_X; ++i)
+	{
+		for (int j = 0; j < max_Y; ++i)
+		{
+			
+		}
+	}
+	*/
+
+	Conj<Coordenada>::const_Iterador it = m.coordenadas();
+	while(it.HaySiguiente())
+	{
+		os << it.Siguiente();
+		it.Avanzar();
+	}
+
+
+
+
+}
+
+
+#endif
+
+
