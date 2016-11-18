a0bb9e46ba68ae4a30c37a8988b54a4786942ea7
istng <ivanstng@mail.com>
2016-11-18 18:24:10 -0300

    agrego heap con mas cosas, y sus tests

--- Base/HeapModificable.h
+++ Base/HeapModificable.h
@@ -3,6 +3,11 @@
 
 #include "aed2/TiposBasicos.h"
 
+#include <iostream>
+#include "mini_test.h"
+
+using namespace std;
+
 using namespace aed2;
 
 class HeapModificable
@@ -116,23 +121,37 @@ HeapModificable::HeapModificable() : tope(NULL) {}
 
 HeapModificable::Iterador HeapModificable::encolar(const JugadorHeap& a)
 {
+	////cout << endl << "enc1" << endl;
     Nodo* siguienteIt;
+    ////cout << "enc2" << endl;
 	if(tope == NULL){
+		////cout << "enc3" << endl;
 		tope = new Nodo(a, 0, 0, NULL, NULL, NULL);
+		////cout << "enc4" << endl;
 		siguienteIt = tope;
 	}
 	else {
+		////cout << "enc5" << endl;
 		Nodo* ftrPadre = futuroPadre();
+		////cout << "enc6" << endl;
 		if(ftrPadre->hijoIzq == NULL){
-			ftrPadre->hijoIzq = new Nodo(a, 0, 0, NULL, NULL, NULL);
+			////cout << "enc7" << endl;
+			ftrPadre->hijoIzq = new Nodo(a, 0, 0, NULL, NULL, ftrPadre);
+			////cout << "enc8" << endl;
 			ftrPadre = ftrPadre->hijoIzq;
+
 		}
 		else {
-			ftrPadre->hijoDer = new Nodo(a, 0, 0, NULL, NULL, NULL);
+			////cout << "enc9" << endl;
+			ftrPadre->hijoDer = new Nodo(a, 0, 0, NULL, NULL, ftrPadre);
+			////cout << "enc10" << endl;
 			ftrPadre = ftrPadre->hijoDer;
 		}
+		////cout << "enc11" << endl;
 		corregirProfundidad(ftrPadre->padre);
+		////cout << "enc12" << endl;
 		siftUp(ftrPadre);
+		////cout << "enc13" << endl;
 		siguienteIt = ftrPadre;
 	}
 	return Iterador(this, siguienteIt);
@@ -141,31 +160,56 @@ HeapModificable::Iterador HeapModificable::encolar(const JugadorHeap& a)
 // Otras operaciones
 void HeapModificable::desencolar()
 {
+	cout << "desenc1" << endl;
 	Nodo* destruir = tope;
+	cout << "desenc2" << endl;
 	if ((*tope).hijoIzq == NULL && (*tope).hijoDer == NULL)
 	{
+		cout << "desenc3" << endl;
 		tope = NULL;
 	}
 	else
 	{
+		cout << "desenc4" << endl;
 		Nodo* ultNodo = ultimoNodo();
+		bool JUAB = false;
+		if (ultNodo == NULL)
+		{
+			JUAB = true;
+		}
+		cout << JUAB << endl;
+		cout << "desenc5" << endl;
 		Nodo* padreUlt = (*ultNodo).padre;
+		cout << "desenc6" << endl;
 		if ((*padreUlt).hijoDer == ultNodo)
 		{
+			cout << "desenc7" << endl;
 			(*padreUlt).hijoDer = NULL;
 		}
 		else
 		{
+			cout << "desenc8" << endl;
 			(*padreUlt).hijoIzq = NULL;
 		}
+		cout << "desenc9" << endl;
 		corregirProfundidad(padreUlt);
+		cout << "desenc10" << endl;
 		(*ultNodo).padre = NULL;
+		cout << "desenc11" << endl;
 		(*ultNodo).hijoIzq = (*tope).hijoIzq;
+		cout << "desenc12" << endl;
 		(*ultNodo).hijoDer = (*tope).hijoDer;
+		cout << "desenc13" << endl;
 		(*ultNodo).ramaMasCorta = (*tope).ramaMasCorta;
+		cout << "desenc14" << endl;
 		(*ultNodo).ramaMasLarga = (*tope).ramaMasLarga;
+		cout << "desenc15" << endl;
 		tope = ultNodo;
-		siftDown(tope);
+		cout << "desenc16" << endl;
+		if ((*tope).hijoIzq != NULL || (*tope).hijoDer != NULL)
+		{
+			siftDown(tope);
+		}
 	}
 	delete destruir;
 }
@@ -173,9 +217,13 @@ void HeapModificable::desencolar()
 // Destructor
 HeapModificable::~HeapModificable()
 {
+	////cout << "LUCAS NO TIENE RAZON" << endl;
     while(tope != NULL){
+    	////cout << "LUCAS NO TIENE RAZON" << endl;
         desencolar();
+        ////cout << "LUCAS NO TIENE RAZON" << endl;
     }
+    ////cout << "SE EQUIVOCO" << endl;
 }
 
 // Iterador
@@ -195,39 +243,62 @@ HeapModificable::JugadorHeap& HeapModificable::Iterador::Siguiente() const
 
 void HeapModificable::Iterador::eliminarSiguiente()
 {
+	//cout << "elSIG1" << endl;
 	Nodo* ultimoNodo = (*heap).tope;
+	//cout << "elSIG2" << endl;
 	if ((*ultimoNodo).hijoIzq == NULL && (*ultimoNodo).hijoDer == NULL)
 	{
+		//cout << "elSIG3" << endl;
 		(*heap).tope = NULL;
 	}
+	////cout << "elSIG4" << endl;
 	else
 	{
+		//cout << "elSIG5" << endl;
 		ultimoNodo = heap->ultimoNodo();	//en diseño: quedo de c, era del heap del it
+		//cout << "elSIG6" << endl;
 		Nodo* padreUlt = (*ultimoNodo).padre;
+		//cout << "elSIG7" << endl;
 		if ((*padreUlt).hijoDer == ultimoNodo)
 		{
+			//cout << "elSIG7" << endl;
 			(*padreUlt).hijoDer = NULL;
 		}
+		////cout << "elSIG8" << endl;
 		else
 		{
+			//cout << "elSIG9" << endl;
 			(*padreUlt).hijoIzq = NULL;
 		}
+		//cout << "elSIG10" << endl;
 		heap->corregirProfundidad(padreUlt);	//idem
+		//cout << "elSIG11" << endl;
 		(*ultimoNodo).padre = (*siguiente).padre;
+		//cout << "elSIG12" << endl;
 		(*ultimoNodo).hijoIzq = (*siguiente).hijoIzq;
+		//cout << "elSIG13" << endl;
 		(*ultimoNodo).hijoDer = (*siguiente).hijoDer;
+		//cout << "elSIG14" << endl;
 		(*ultimoNodo).ramaMasCorta = (*siguiente).ramaMasCorta;
+		//cout << "elSIG15" << endl;
 		(*ultimoNodo).ramaMasLarga = (*siguiente).ramaMasLarga;
+		//cout << "elSIG16" << endl;
 		if ((*(*siguiente).padre).hijoIzq == siguiente)
 		{
+			//cout << "elSIG17" << endl;
 			(*(*siguiente).padre).hijoIzq = ultimoNodo;
 		}
+		////cout << "elSIG18" << endl;
 		else
 		{
+			//cout << "elSIG19" << endl;
 			(*(*siguiente).padre).hijoDer = ultimoNodo;
 		}
+		//cout << "elSIG20" << endl;
 		delete siguiente;
+		//cout << "elSIG21" << endl;
 		heap->siftDown(ultimoNodo);
+		//cout << "elSIG22" << endl;
 		heap->siftUp(ultimoNodo);
 	}
 }
@@ -257,33 +328,48 @@ const HeapModificable::JugadorHeap& HeapModificable::const_Iterador::Siguiente()
 
 HeapModificable::Nodo* HeapModificable::ultimoNodo() const 
 {
+	////cout << "ult1" << endl;
 	Nodo* ultimoNodo = tope;
-	while (!((*ultimoNodo).hijoIzq == NULL && (*ultimoNodo).hijoDer == NULL))
+	////cout << "ult2" << endl;
+	while ((*ultimoNodo).hijoIzq != NULL && (*ultimoNodo).hijoDer != NULL)
 	{
-		if ( (ultimoNodo->hijoIzq)->ramaMasLarga == (ultimoNodo->hijoDer)->ramaMasLarga )
+		////cout << "ult3" << endl;
+		if ( (*(*ultimoNodo).hijoIzq).ramaMasLarga == (*(*ultimoNodo).hijoDer).ramaMasLarga )
 		{
+			////cout << "ult4" << endl;
 			ultimoNodo = (*ultimoNodo).hijoDer;
 		}
 		else
 		{
+			////cout << "ult5" << endl;
 			ultimoNodo = (*ultimoNodo).hijoIzq;
 		}
 	}
+	if ((*ultimoNodo).hijoDer != NULL)
+	{
+		ultimoNodo = (*ultimoNodo).hijoDer;
+	}
+	else if((*ultimoNodo).hijoIzq != NULL)
+	{
+		ultimoNodo = (*ultimoNodo).hijoIzq;
+	}
+	////cout << "ult6" << endl;
+
 	return ultimoNodo;
 }
 
 HeapModificable::Nodo* HeapModificable::futuroPadre() const 
 {
 	Nodo* ultimoNodo = tope;
-	while (!((*ultimoNodo).hijoIzq == NULL && (*ultimoNodo).hijoDer == NULL))
+	while ((*ultimoNodo).hijoIzq != NULL && (*ultimoNodo).hijoDer != NULL)
 	{
-		if ( (ultimoNodo->hijoIzq)->ramaMasLarga == (ultimoNodo->hijoDer)->ramaMasLarga )
+		if ( (ultimoNodo->hijoIzq)->ramaMasCorta == (ultimoNodo->hijoDer)->ramaMasCorta )
 		{
-			ultimoNodo = (*ultimoNodo).hijoDer;
+			ultimoNodo = (*ultimoNodo).hijoIzq;
 		}
 		else
 		{
-			ultimoNodo = (*ultimoNodo).hijoIzq;
+			ultimoNodo = (*ultimoNodo).hijoDer;
 		}
 	}
 	return ultimoNodo;
@@ -291,60 +377,105 @@ HeapModificable::Nodo* HeapModificable::futuroPadre() const
 
 void HeapModificable::corregirProfundidad(Nodo* p)
 {
+	////cout << "corr1" << endl;
 	if ((*p).hijoIzq == NULL && (*p).hijoDer == NULL)
 	{
+		////cout << "corr2" << endl;
 		(*p).ramaMasCorta = 0;
 		(*p).ramaMasLarga = 0;
 	}
 	else
 	{
+		////cout << "corr3" << endl;
 		if ((*p).hijoIzq != NULL && (*p).hijoDer != NULL)
 		{
+			////cout << "corr4" << endl;
 			(*p).ramaMasCorta = 1;
 			(*p).ramaMasLarga = 1;
 		}
 		else
 		{
+			////cout << "corr5" << endl;
 			(*p).ramaMasCorta = 0;
 			(*p).ramaMasLarga = 1;
 		}
 	}
-	p = (*p).padre;
+	////cout << "corr6" << endl;
+	////cout << "SON UNSIGNGSSEEDDEED" << endl;
 	while((*p).padre != NULL)
 	{
-		(*p).ramaMasCorta = (*((*p).hijoDer)).ramaMasCorta + 1;
-		(*p).ramaMasLarga = (*((*p).hijoIzq)).ramaMasLarga + 1;
+		////cout << "sera aca?" << endl;
+		p = (*p).padre;
+		if ((*p).hijoDer != NULL)
+		{
+			(*p).ramaMasCorta = (*((*p).hijoDer)).ramaMasCorta + 1;
+		}
+		if ((*p).hijoIzq != NULL)
+		{
+			(*p).ramaMasLarga = (*((*p).hijoIzq)).ramaMasLarga + 1;
+		}
+		
 	}
+	////cout << "corr324132143214" << endl;
 }
 
 void HeapModificable::siftDown(Nodo* p)
 {
+	//cout << "siftd1" << endl;
 	Nodo* swap = p;
-	if (((*(*p).hijoIzq).elemento < (*p).elemento))
+	//cout << "siftd2" << endl;
+	if ((*p).hijoIzq != NULL )
 	{
+		if (((*(*p).hijoIzq).elemento < (*p).elemento))
+		{
+		//cout << "siftd3" << endl;
 		swap = (*p).hijoIzq;
+		}
 	}
-	if ((*(*p).hijoDer).elemento < (*p).elemento)
+
+	if ((*p).hijoDer != NULL)
 	{
-		swap = (*p).hijoDer;
+		if ((*(*p).hijoDer).elemento < (*p).elemento)
+		{
+			//cout << "siftd5" << endl;
+			swap = (*p).hijoDer;
+		}
 	}
+	//cout << "siftd4" << endl;
+
+	//cout << "siftd6" << endl;
 	if (p != swap)
 	{
+		//cout << "siftd7" << endl;
 		intercambio(p, swap);
+		//cout << "siftd8" << endl;
 		siftDown(swap);
 	}
 }
 
 void HeapModificable::siftUp(Nodo* p)
 {
+	//cout << "siftu1" << endl;
 	Nodo* swap = p;
+	//cout << "siftu2" << endl;
 	if(p->elemento < (p->padre)->elemento){
+		//cout << "siftu3" << endl;
 		swap = p->padre;
 	}
+	//cout << "siftu4" << endl;
 	if(p != swap){
+		//cout << "siftu5" << endl;
 		intercambio(swap, p);
+		//cout << "siftu6" << endl;
 		siftUp(swap);
 	}
+	//cout << "siftu7" << endl;
+	if ((*p).padre == NULL)
+	{
+		//cout << "siftu8" << endl;
+		tope = p;
+	}
+	
 }
 
 void HeapModificable::intercambio(Nodo* padre, Nodo* hijo)
---
+++ Base/testsHeap.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "mini_test.h"
+
+#include "HeapModificable.h"
+
+using namespace std;
+
+//auxiliares
+
+bool jugHeapIgual(HeapModificable::JugadorHeap j1, HeapModificable::JugadorHeap j2)
+{
+	return j1.cantPokes == j2.cantPokes && j1.id == j2.id;
+}
+
+//Chequea que el diccionario vacio no tenga claves
+void es_vacia() {
+	HeapModificable h;
+	ASSERT(h.esVacia());
+}
+
+void encolar() {
+
+	HeapModificable h;
+
+	HeapModificable::JugadorHeap j0 = HeapModificable::JugadorHeap(3, 0);
+
+	h.encolar(j0);
+	//cout << endl << "assert0" << endl;
+	ASSERT(jugHeapIgual(h.proximo(), j0));
+	//cout << endl << "creacion jh" << endl;
+	HeapModificable::JugadorHeap j1 = HeapModificable::JugadorHeap(2, 1);
+	HeapModificable::JugadorHeap j2 = HeapModificable::JugadorHeap(0, 2);
+	HeapModificable::JugadorHeap j3 = HeapModificable::JugadorHeap(4, 3);
+		//cout << endl << "encolado 1" << endl;
+	h.encolar(j1);
+				//cout << h.proximo().cantPokes << " " << h.proximo().id << endl;
+		//cout << j2.cantPokes << " " << j2.id << endl;
+		//cout << endl << "encolado 2" << endl;
+	h.encolar(j2);
+			//cout << h.proximo().cantPokes << " " << h.proximo().id << endl;
+		//cout << j2.cantPokes << " " << j2.id << endl;
+		//cout << endl << "encolado 3" << endl;
+	h.encolar(j3);
+		//cout << endl << "assert1" << endl;
+		//cout << h.proximo().cantPokes << " " << h.proximo().id << endl;
+		//cout << j2.cantPokes << " " << j2.id << endl;
+	ASSERT(jugHeapIgual(h.proximo(), j2));
+	//cout << "imposible" << endl;
+}
+
+void eliminar_siguiente()
+{
+	HeapModificable h;
+
+	HeapModificable::JugadorHeap j0 = HeapModificable::JugadorHeap(3, 0);
+
+	h.encolar(j0);
+	//cout << endl << "assert0" << endl;
+	ASSERT(jugHeapIgual(h.proximo(), j0));
+	//cout << endl << "creacion jh" << endl;
+	HeapModificable::JugadorHeap j1 = HeapModificable::JugadorHeap(2, 1);
+	HeapModificable::JugadorHeap j2 = HeapModificable::JugadorHeap(0, 2);
+	HeapModificable::JugadorHeap j3 = HeapModificable::JugadorHeap(4, 3);
+		//cout << endl << "encolado 1" << endl;
+	HeapModificable::Iterador itj1 = h.encolar(j1);
+				//cout << h.proximo().cantPokes << " " << h.proximo().id << endl;
+		//cout << j2.cantPokes << " " << j2.id << endl;
+		//cout << endl << "encolado 2" << endl;
+	h.encolar(j2);
+			//cout << h.proximo().cantPokes << " " << h.proximo().id << endl;
+		//cout << j2.cantPokes << " " << j2.id << endl;
+		//cout << endl << "encolado 3" << endl;
+	h.encolar(j3);
+	cout << endl << "elsig1" << endl;
+	itj1.eliminarSiguiente();
+	cout << "elsig2" << endl;
+	ASSERT(jugHeapIgual(h.proximo(), j2));
+	cout << "elsig3" << endl;
+	h.desencolar();
+	cout << "elsig4" << endl;
+	ASSERT(jugHeapIgual(h.proximo(), j0));
+	cout << "elsig5" << endl;
+	h.desencolar();
+	cout << "elsig6" << endl;
+	ASSERT(jugHeapIgual(h.proximo(), j3));
+	cout << "elsig7" << endl;
+	h.desencolar();
+	cout << "elsig8" << endl;
+	ASSERT(h.esVacia());
+
+}
+
+int main() {
+	RUN_TEST(es_vacia);
+	RUN_TEST(encolar);
+	RUN_TEST(eliminar_siguiente);
+
+	return 0;
+
+}
