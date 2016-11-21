b2d7b6340d0d401d96ce731b52dfa11c6e49918c
istng <ivanstng@mail.com>
2016-11-21 20:56:04 -0300

    agrego errores comentados en testsHeap.cpp

--- Base/testsHeap.cpp
+++ Base/testsHeap.cpp
@@ -19,6 +19,63 @@ bool jugHeapIgual(HeapModificable::JugadorHeap j1, HeapModificable::JugadorHeap 
 void es_vacia() {
 	HeapModificable h;
 	ASSERT(h.esVacia());
+
+	HeapModificable::JugadorHeap j0 = HeapModificable::JugadorHeap(3, 0);
+	HeapModificable::JugadorHeap j1 = HeapModificable::JugadorHeap(2, 1);
+	HeapModificable::JugadorHeap j2 = HeapModificable::JugadorHeap(0, 2);
+	
+	h.encolar(j0);
+	h.encolar(j1);
+	h.encolar(j2);
+	h.desencolar();
+	h.desencolar();
+	h.desencolar();
+	ASSERT(h.esVacia());
+
+	HeapModificable l;
+	l.encolar(j0);
+	l.encolar(j2);
+	l.encolar(j1);
+	l.desencolar();
+	l.desencolar();
+	l.desencolar();
+	ASSERT(l.esVacia());
+
+	HeapModificable g;
+	g.encolar(j1);
+	g.encolar(j0);
+	g.encolar(j2);
+	g.desencolar();
+	g.desencolar();
+	g.desencolar();
+	ASSERT(g.esVacia());
+
+	HeapModificable m;
+	m.encolar(j1);
+	m.encolar(j2);
+	m.encolar(j0);
+	m.desencolar();
+	m.desencolar();
+	m.desencolar();
+	ASSERT(m.esVacia());
+
+	HeapModificable k;
+	k.encolar(j2);
+	k.encolar(j0);
+	k.encolar(j1);
+	k.desencolar();
+	k.desencolar();
+	k.desencolar();
+	ASSERT(k.esVacia());
+
+	HeapModificable n;
+	n.encolar(j2);
+	n.encolar(j1);
+	n.encolar(j0);
+	n.desencolar();
+	n.desencolar();
+	n.desencolar();
+	ASSERT(n.esVacia());
 }
 
 void encolar() {
@@ -92,29 +149,29 @@ void siguiente()
 
 	itj1.eliminarSiguiente();
 	ASSERT(jugHeapIgual(itj0.Siguiente(), j0));
-	ASSERT(!jugHeapIgual(itj1.Siguiente(), j1));
+	ASSERT(jugHeapIgual(itj1.Siguiente(), j1));	//Si esto esta !, tira true en ./ y false en val, y visebersa si no esta el !
 	ASSERT(jugHeapIgual(itj2.Siguiente(), j2));
 	ASSERT(jugHeapIgual(itj2.Siguiente(), h.proximo()));
 	ASSERT(jugHeapIgual(itj3.Siguiente(), j3));
-
+/*
 	itj2.eliminarSiguiente();
 	ASSERT(jugHeapIgual(itj0.Siguiente(), j0));
 	ASSERT(jugHeapIgual(itj0.Siguiente(), h.proximo()));
 	ASSERT(!jugHeapIgual(itj2.Siguiente(), j2));
 	ASSERT(!jugHeapIgual(itj2.Siguiente(), h.proximo()));
 	ASSERT(jugHeapIgual(itj3.Siguiente(), j3));
-
+*/
 }
 
 void agregar_como_siguiente()
-{/*
+{
 	HeapModificable h;
-*/
+
 	HeapModificable::JugadorHeap j0 = HeapModificable::JugadorHeap(3, 0);
 	HeapModificable::JugadorHeap j1 = HeapModificable::JugadorHeap(2, 1);
 	HeapModificable::JugadorHeap j2 = HeapModificable::JugadorHeap(0, 2);
 	HeapModificable::JugadorHeap j3 = HeapModificable::JugadorHeap(4, 3);
-/*
+
 	HeapModificable::Iterador itj0 = h.encolar(j0);
 	HeapModificable::Iterador itj3 = h.encolar(j3);
 
@@ -124,25 +181,24 @@ void agregar_como_siguiente()
 	itj0.agregarComoSiguiente(j2);
 	ASSERT(!jugHeapIgual(h.proximo(), j1));
 	ASSERT(jugHeapIgual(h.proximo(), j2));
-*/
 
 
 	HeapModificable g;
-	cout << endl << "agsig" << __LINE__ << endl;
+	//cout << endl << "agsig" << __LINE__ << endl;
 	g.encolar(j1);
-	cout << "agsig" << __LINE__ << endl;
+	//cout << "agsig" << __LINE__ << endl;
 	ASSERT(jugHeapIgual(g.proximo(), j1));
-	cout << "agsig" << __LINE__ << endl;
+	//cout << "agsig" << __LINE__ << endl;
 	HeapModificable::Iterador itg3 = g.encolar(j3);
-	cout << "agsig" << __LINE__ << endl;
+	//cout << "agsig" << __LINE__ << endl;
 	ASSERT(jugHeapIgual(g.proximo(), j1));
-	cout << "agsig" << __LINE__ << endl;
+	//cout << "agsig" << __LINE__ << endl;
 	g.encolar(j2);
-	cout << "agsig" << __LINE__ << endl;
+	//cout << "agsig" << __LINE__ << endl;
 	ASSERT(jugHeapIgual(g.proximo(), j2));
-	cout << "agsig" << __LINE__ << endl;
+	//cout << "agsig" << __LINE__ << endl;
 	itg3.agregarComoSiguiente(j0);
-	cout << "agsig" << __LINE__ << endl;
+	//cout << "agsig" << __LINE__ << endl;
 	ASSERT(jugHeapIgual(g.proximo(), j2));
 
 }
@@ -188,9 +244,9 @@ int main() {
 	//RUN_TEST(es_vacia);
 	//RUN_TEST(encolar);
 	//RUN_TEST(eliminar_siguiente);
-	//RUN_TEST(siguiente);
-	//RUN_TEST(agregar_como_siguiente);
-	RUN_TEST(desencolar);
+	RUN_TEST(siguiente);	//tiene un error, alterna resultado con !
+	RUN_TEST(agregar_como_siguiente);	//no libera y tiene dos errores
+	//RUN_TEST(desencolar);
 	return 0;
 
 }
